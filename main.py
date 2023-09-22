f1b85cbb0344bde7e020b6391928bc6edd541ab9
jeremiah.1582@googlemail.com <jeremiah.1582@googlemail.com>
2023-09-22 17:11:26 +0200

    formating

--- main.py
+++ main.py
@@ -13,12 +13,14 @@ How the code will work:
 # ...................................write code above...................................
 
 # remember syntax:
-# 
-# If condition_is_TRUE:
-#   execute code...
-# elif:
-#   execute code...
-# else:
+
+    # If condition_is_TRUE:
+    #   execute code...
+    # elif:
+    #   execute code...
+    # else:
+    
+
 
 
 
